cdf8928280967be961d5ff03bc86d82aa754ea8d
Daria Jung <djj2115cu@gmail.com>
2014-06-23 15:32:55 -0400

    moar unit tests

--- unit_tests.hs
+++ unit_tests.hs
@@ -1,5 +1,7 @@
 import Sudoku
 import Test.QuickCheck
+import Data.Map as Map
+import Data.Set as Set
 
 
 -- Unit Tests --
@@ -9,5 +11,8 @@ check_length_unitlist = length unitlist == 27
 
 check_length_units = [(length . lookup' s $ units) == 3 | s <- squares]
 
-check_c2 = lookup' ('C', '2') units == 
+check_c2_units = lookup' ('C', '2') units == 
     [[('A','2'),('B','2'),('C','2'),('D','2'),('E','2'),('F','2'),('G','2'),('H','2'),('I','2')],[('C','1'),('C','2'),('C','3'),('C','4'),('C','5'),('C','6'),('C','7'),('C','8'),('C','9')],[('A','1'),('A','2'),('A','3'),('B','1'),('B','2'),('B','3'),('C','1'),('C','2'),('C','3')]]
+
+check_c2_peers = lookup' ('C', '2') peers ==
+    Set.fromList [('A','1'),('A','2'),('A','3'),('B','1'),('B','2'),('B','3'),('C','1'),('C','3'),('C','4'),('C','5'),('C','6'),('C','7'),('C','8'),('C','9'),('D','2'),('E','2'),('F','2'),('G','2'),('H','2'),('I','2')]
