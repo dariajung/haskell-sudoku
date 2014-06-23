810a619afc4a31a57fea4937577b87337c484ba8
Daria Jung <djj2115cu@gmail.com>
2014-06-23 15:06:47 -0400

    get setup working

--- sudoku.hs
+++ sudoku.hs
@@ -1,10 +1,16 @@
 import Data.List
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Maybe
 
 -- set types
 type Digit = Char
 type Square = (Char, Char)
 type Unit = [Square]
 
+digits   = "123456789"
+rows     = "ABCDEFGHI"
+cols     = digits
 
 cross :: String -> String -> [Square]
 cross xs ys = [ (x, y) | x <- xs, y <- ys]
@@ -12,12 +18,17 @@ cross xs ys = [ (x, y) | x <- xs, y <- ys]
 squares :: [Square]
 squares = cross rows cols
 
-digits   = "123456789"
-rows     = "ABCDEFGHI"
-cols     = digits
+unitlist :: [Unit]
+unitlist = 
+        [cross rows [c] | c <- cols] ++
+        [cross [r] cols | r <- rows] ++
+        [cross rs cs | rs <- ["ABC", "DEF", "GHI"], cs <- ["123", "456", "789"]]
 
+units :: Map.Map Square [Unit]
+units = Map.fromList [(s, [filter (/= s) u | u <- unitlist, s `elem` u]) | s <- squares]
 
---unitlist = ([cross(rows, c) for c in cols] +
---            [cross(r, cols) for r in rows] +
---            [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
+lookup' :: Ord k => k -> Map.Map k c -> c
+lookup' key _map = fromJust . Map.lookup key $ _map
 
+peers :: Map.Map Square (Set Square)
+peers = Map.fromList [(s, Set.difference (Set.fromList . concat . lookup' s $ units) (Set.fromList [s])) | s <- squares]
