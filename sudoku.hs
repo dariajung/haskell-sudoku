96293c904ebd09e33c3865c2469244b4e415300c
Daria Jung <djj2115cu@gmail.com>
2014-06-23 16:44:50 -0400

    working on eliminate func

--- sudoku.hs
+++ sudoku.hs
@@ -9,6 +9,7 @@ import Data.Maybe
 type Digit = Char
 type Square = (Char, Char)
 type Unit = [Square]
+type Grid = Map.Map Square Digit
 
 digits   = "123456789"
 rows     = "ABCDEFGHI"
@@ -35,9 +36,27 @@ lookup' key _map = fromJust . Map.lookup key $ _map
 peers :: Map.Map Square (Set.Set Square)
 peers = Map.fromList [(s, Set.difference (Set.fromList . concat . lookup' s $ units) (Set.fromList [s])) | s <- squares]
 
-
 gridValues :: [Char] -> Map.Map Square Char
 gridValues grid = 
     let chars = [c | c <- grid, c `elem` digits || c `elem` "0."]
     in Map.fromList $ zip squares chars
+
+-- values = dict((s, digits) for s in squares)
+--gridParse grid = 
+--    let values = Map.fromList [(s, digits) | s <- squares]
+        
+
+eliminate values s d = 
+    if d `elem` (lookup' values s) == False then values
+    else 
+    let newV = Map.update (\vals -> Just $ del d vals) s values
+
+    -- (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+    propagate_peers vs 
+        | length (lookup' s vs) == 0    = Nothing
+        | length (lookup' s vs) == 1    =
+            let d2 = (lookup' s vs)
+
+
+
 
