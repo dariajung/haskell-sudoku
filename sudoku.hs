b9418052b8905397d84591bbc7aa218c7d44b816
Daria Jung <djj2115cu@gmail.com>
2014-06-24 14:26:17 -0400

    found bug...

--- sudoku.hs
+++ sudoku.hs
@@ -51,37 +51,39 @@ parseGrid str =
 
 eliminate :: Grid -> Square -> Char -> Maybe Grid
 eliminate values s d = 
-    if (d `elem` (lookup' s values)) == False then Just values
+    if (d `elem` (values Map.! s)) == False then Just values -- Already eliminated 
     else 
     let newV = Map.update (\vals -> Just $ delete d vals) s values
 
-    -- (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
-        propagate_peers vs 
-            | length (vs Map.! s) == 0    = Nothing
-            | length (vs Map.! s) == 1    =
-                let d2 = (vs Map.! s) !! 0 -- vs Map.! s should be length 1
-                    inner_prop_peers values s2 = eliminate values s2 d2 
-                in foldM inner_prop_peers vs (Set.toList $ peers Map.! s)
-            | otherwise                    = Just vs
-
-    -- (2) If a unit u is reduced to only one place for a value d, then put it there.
-        propagate_units vs =
-            let reduce_prop vs' u = inner_prop_units vs' u              
-                inner_prop_units vals unit
-                    | length (dplaces) == 0 = Nothing
-                    | length (dplaces) == 1 = assign vals (dplaces !! 0) d
-                    | otherwise             = Just vals
-                    where dplaces = [sq | sq <- unit, d `elem` (vals Map.! s)]
-
-            in foldM reduce_prop vs (units Map.! s)
-
     in do
-        vs' <- propagate_peers newV
-        result <- propagate_units vs'
+        vs' <- propagatePeers newV s d
+        result <- propagateUnits vs' s d
         return result
+
+propagatePeers :: Grid -> Square -> Digit -> Maybe Grid
+propagatePeers vs s d
+    -- (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+    | length (vs Map.! s) == 0    = Nothing -- contradiction: removed last value
+    | length (vs Map.! s) == 1    =
+        let d2 = (vs Map.! s) !! 0 -- vs Map.! s should be length 1
+            inner_prop_peers values s2 = eliminate values s2 d2 
+        in foldM inner_prop_peers vs (Set.toList $ peers Map.! s)
+    | otherwise                    = Just vs
+
+propagateUnits :: Grid -> Square -> Digit -> Maybe Grid
+propagateUnits vs s d = 
+    -- (2) If a unit u is reduced to only one place for a value d, then put it there.
+    let reduceProp vs' u = inner_prop_units vs' u              
+        inner_prop_units vals unit
+            | length (dplaces) == 0 = Nothing -- contradiction
+            | length (dplaces) == 1 = assign vals (dplaces !! 0) d
+            | otherwise             = Just vals
+            where dplaces = [sq | sq <- unit, d `elem` (vals Map.! sq)]
+
+    in foldM reduceProp vs (units Map.! s)
 
 assign :: Grid -> Square -> Digit -> Maybe Grid
 assign values s d = 
-    let other_values = (delete d $ values Map.! s)
+    let otherValues = (delete d $ values Map.! s)
         reduce vals dig = eliminate vals s dig
-    in foldM reduce values other_values
+    in foldM reduce values otherValues
