cd1b1cc1566f0f629bb593fd7cc8ca4c477cec34
Daria Jung <djj2115cu@gmail.com>
2014-06-23 21:18:40 -0400

    yay type checks

--- sudoku.hs
+++ sudoku.hs
@@ -9,7 +9,7 @@ import Data.Maybe
 type Digit = Char
 type Square = (Char, Char)
 type Unit = [Square]
-type Grid = Map.Map Square Digit
+type Grid = Map.Map Square [Char]
 
 digits   = "123456789"
 rows     = "ABCDEFGHI"
@@ -39,46 +39,50 @@ peers = Map.fromList [(s, Set.difference (Set.fromList . concat . lookup' s $ un
 gridValues :: [Char] -> Map.Map Square Char
 gridValues grid = 
     let chars = [c | c <- grid, c `elem` digits || c `elem` "0."]
-    in Map.fromList $ zip squares chars
+    in Map.fromList (zip squares chars)
 
 -- values = dict((s, digits) for s in squares)
+-- gridParse :: [Char] -> Grid
 --gridParse grid = 
 --    let values = Map.fromList [(s, digits) | s <- squares]
         
-
+eliminate :: Grid -> Square -> Char -> Maybe Grid
 eliminate values s d = 
-    if (d `elem` (lookup' s values)) == False then values
+    if (d `elem` (lookup' s values)) == False then Just values
     else 
     let newV = Map.update (\vals -> Just $ delete d vals) s values
 
     -- (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
         propagate_peers vs 
-            | length (lookup' s vs) == 0    = Nothing
-            | length (lookup' s vs) == 1    =
-                let d2 = (vs Map.! s)
+            | length (vs Map.! s) == 0    = Nothing
+            | length (vs Map.! s) == 1    =
+                let d2 = (vs Map.! s) !! 0
                 --  not all(eliminate(values, s2, d2) for s2 in peers[s]):
-                    inner_prop_peers = if (all (==True) (map (==Nothing) [eliminate vs s2 d2 | s2 <- Set.toList $ peers Map.! s])) == False then Nothing
-                    else Just vs
+                    --inner_prop_peers = if (all (==True) (map (/=Nothing) [eliminate vs s2 d2 | s2 <- Set.toList $ peers Map.! s])) == False then Nothing
+                    --else Just vs
+                    inner_prop_peers
+                        | (all (==True) (map (/=Nothing) [eliminate vs s2 d2 | s2 <- Set.toList $ peers Map.! s])) == False = Nothing
                 in inner_prop_peers
 
     -- (2) If a unit u is reduced to only one place for a value d, then put it there.
-        propagate_units vs u = 
+        propagate_units vs = 
         -- [s for s in u if d in values[s]]
-            let dplaces = [s | s <- u, d `elem` (vs Map.! s)]
+            let u = concat ([x | x <- units Map.! s])
+                dplaces = [y | y <- u, d `elem` (concat $ Map.fold (:) [] vs)]
                 inner_prop_units 
                     | length (dplaces) == 0 = Nothing
                     | length (dplaces) == 1 = assign vs (dplaces !! 0) d
-                    | otherwise             = Just vs
+                    
+                    -- | otherwise             = Just vs
             in inner_prop_units
 
     in do
-    vs' <- propagate_peers newV
-    result <- propagate_units vs' ([u | u <- units Map.! s])
-    return result
+        vs' <- propagate_peers newV
+        result <- propagate_units vs'
+        return result
 
-
-
-
-
-
-
+assign :: Grid -> Square -> Digit -> Maybe Grid
+assign values s d = 
+    let other_values = Map.update (\vals -> Just $ delete d vals) s values
+    in if (all (==True) (map (/=Nothing) [eliminate values s d2 | d2 <- (concat $ Map.fold (:) [] other_values)])) == False then Nothing
+        else Just values
